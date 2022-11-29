5c14f5fd1f39d93ccf1aa3615e3eaae832c7c2df
erickxllx <banegaserick@gmail.com>
2022-11-28 18:57:46 -0700

    Update

--- CentroClinico/settings.py
+++ CentroClinico/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'centroclinico',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': '', 
         'HOST':'localhost',
         'PORT': '3307',
     }
