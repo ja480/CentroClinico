9d8a43afbbc08ba8d92db015369ff1a9128695af
erickxllx <banegaserick@gmail.com>
2022-12-03 12:29:43 -0700

    Login ready

--- CentroClinico/views.py
+++ CentroClinico/views.py
@@ -92,29 +92,21 @@ def login(request):
 	error = ""
 
 	if request.method == 'POST':
-		u = request.POST['email']
-		p = request.POST['password']
+		u = request.POST.get['email']
+		p = request.POST.get['password']
 		user = authenticate(request,username=u,password=p)
 		try:
 			if user is not None:
 				login(request,user)
 				error = "no"
-				g = request.user.groups.all()[0].name
-				if g == 'Doctor':
-					page = "doctor"
-					d = {'error': error,'page':page}
-					return render(request,'index.html',d)
-				elif g == 'Receptionist':
-					page = "reception"
-					d = {'error': error,'page':page}
-					return render(request,'index.html',d)
-				if g == 'Patients':
-					d = {'error': error}
-					return render(request,'index.html',d)
-			else:
-				error = "yes"
+			return render (request,'index.html')	
 		except Exception as e:
 			error = "yes"
 			#print(e)
 			#raise e
 	return render(request,'registration/login.html')
+
+def Logout(request):
+    logout(request)
+    return redirect('index')
+	
